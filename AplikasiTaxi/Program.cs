f231e19f3d897cdba62a36d52032d87dcda7730b
galih027 <galihanggriawan@students.amikom.ac.id>
2022-03-23 18:32:00 +0700

    TAXI

---
+++ AplikasiTaxi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //Pengesetan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // Pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey(); 
+        }
+    }
+}
